64f73279e25b12547ba82d67fcd2faac71c347ae
NhatCuong2511 <124860627+NhatCuong2511@users.noreply.github.com>
2026-02-02 16:56:16 -0500

    final touch

--- car.js
+++ car.js
@@ -10,53 +10,76 @@ class Car{
         this.maxSpeed=maxSpeed;
         this.friction=0.05;
         this.angle=0;
-        this.damaged = false;
+        this.damaged=false;
 
-        this.sensor=new Sensor(this);
+        this.useBrain=controlType=="AI";
+
+        if(controlType!="DUMMY"){
+            this.sensor=new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
+        }
         this.controls=new Controls(controlType);
     }
 
-    update(roadBorders){
+    update(roadBorders,traffic){
         if(!this.damaged){
             this.#move();
-            this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders);
-            
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders,traffic);
         }
-        this.sensor.update(roadBorders);
+        if(this.sensor){
+            this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
 
-    }
-
-    #assessDamage(roadBorders) {
-        for (let i=0;i<roadBorders.length; i++) {
-            if(polysIntersect(this.polygon, roadBorders[i])) {
-                return true
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
             }
         }
-        return false
     }
 
-    #createPolygon() { // replace original draw car method for future detect collision
-      const points = [];
-      const rad = Math.hypot(this.width, this.height)/2;
-      const alpha = Math.atan2(this.width, this.height);
-      points.push({
-        x:this.x-Math.sin(this.angle-alpha)*rad,
-        y:this.y-Math.cos(this.angle-alpha)*rad,
-      });
-      points.push({
-        x:this.x-Math.sin(this.angle+alpha)*rad,
-        y:this.y-Math.cos(this.angle+alpha)*rad,
-      });
-      points.push({
-        x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
-        y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad,
-      });
-      points.push({
-        x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
-        y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad,
-      });
-      return points;
+    #assessDamage(roadBorders,traffic){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        for(let i=0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon,traffic[i].polygon)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
     }
 
     #move(){
@@ -98,18 +121,21 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx,color){
-        if(this.damaged) {
-            ctx.fillStyle = "gray";
-        } else {
-            ctx.fillStyle = color;
+    draw(ctx,color,drawSensor=false){
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle=color;
         }
+        ctx.beginPath();
         ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
         for(let i=1;i<this.polygon.length;i++){
             ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
         }
         ctx.fill();
 
-        this.sensor.draw(ctx);
+        if(this.sensor && drawSensor){
+            this.sensor.draw(ctx);
+        }
     }
 }
